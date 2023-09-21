498e6eebf36dac9db5e115cab5ca402860a8036f
LuisE <luiscv2enrique@gmail.com>
2023-09-21 16:24:45 -0600

    Practica3

---
+++ Practica3/Ejercicio17/DemoPanel.java
@@ -0,0 +1,13 @@
+package Ejercicio17;
+// Fig. 12.46: DemoPanel.java
+// Prueba de MarcoPanel.
+import javax.swing.JFrame;
+
+public class DemoPanel extends JFrame {
+    public static void main(String[] args) { 
+        MarcoPanel marcoPanel = new MarcoPanel(); 
+        marcoPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoPanel.setSize(450, 200);
+        marcoPanel.setVisible(true);
+    }
+} // fin de la clase DemoPanel
